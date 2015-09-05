ff9e7c2cd11738a717fed87bb76eb4b33102f21f
Ratan George Senapathy <ratangsenapathy@gmail.com>
2015-09-05 10:26:21 +0530

    Email feature added

--- client/submitted/submitted.js
+++ client/submitted/submitted.js
@@ -1,3 +1,34 @@
 Meteor.subscribe("assignments");
 Meteor.subscribe("files");
 console.log(Assignments.findOne({_id:window.location.pathname.split("/")[2]}));
+
+Template.submitted.helpers({
+
+	up:function(){
+		
+           var url=window.location.href;
+			var assignmentId=url.substring(url.lastIndexOf('/')+1);
+			var assignments=Assignments.find().fetch();
+			var files=Files.find().fetch();
+			var uploadId="";
+		for(var i=0;i<assignments.length;i++)
+		{
+ 			  if(assignments[i]._id==assignmentId)
+		{
+          		uploadId=assignments[i].upload;
+           		break;
+			}
+		}
+         
+		for(var i=0;i<files.length;i++)
+		{
+       		if(files[i]._id==uploadId)
+       		{     
+                 return files[i].original.name;
+       		}
+	    }
+        
+	    return "";
+	}
+});
+
